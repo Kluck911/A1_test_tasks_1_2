99be559f635fc3d1b7524d35241f8bd4a8f335cc
H_Zavadski <kluck1812@gmail.com>
2022-10-19 19:29:29 +0300

    task 1 completed

--- list_of_apple_phone.py
+++ list_of_apple_phone.py
@@ -1,2 +1,6 @@
 phone_list = ["Xiaomi Redmi Note 10S", "Смартфон Xiaomi Redmi Note 10 Pro", "Apple iPhone 13", "Apple iPhone 11",
               "Huawei nova Y70", "Смартфон Apple iPhone 13 Pro"]
+
+apple_list = (apple for apple in phone_list if "Apple" in apple)
+for i in apple_list:
+    print(i)
